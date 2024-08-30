80b955011070fe456bfc79f0b1aea7b7d4222266
avinash-digicorp <avinash.rathod@digi-corp.com>
2024-08-30 19:14:01 +0530

    api key remove

--- src/config/config-api.tsx
+++ src/config/config-api.tsx
@@ -10,10 +10,7 @@ const WEBSITE = BASE_URL;
 const ENDPOINT_API = BASE_URL + '/api/v1';
 
 //  KEYS
-const GOOGLE_MAP_API_KEY = definePlatformParam(
-  'AIzaSyBFdK5xM1J-aA0ap5upap3XzolW22Gssj0',
-  'AIzaSyBFdK5xM1J-aA0ap5upap3XzolW22Gssj0',
-);
+const GOOGLE_MAP_API_KEY = definePlatformParam('', '');
 export default {
   WEBSITE,
   BASE_URL,
